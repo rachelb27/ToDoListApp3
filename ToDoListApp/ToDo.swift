74db52f862001d16efa35fa6b65e19c96206d60b
Rachel Brous <rachellbrous@gmail.com>
2020-06-23 11:02:10 -0400

    initial commit

---
+++ ToDoListApp/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDoListApp
+//
+//  Created by Rachel Brous on 6/22/20.
+//  Copyright © 2020 Rachel Brous. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
